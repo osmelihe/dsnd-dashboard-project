5d5dd8d6be807f84bc634f04af563a0ad043c187
i5157 <ibrahiimzah@gmail.com>
2026-02-23 22:00:18 +0300

    finished all

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,37 +1,36 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from idlelib.query import Query
+from employee_events.query_base import QueryBase
 
-# Import dependencies for sql execution
-#### YOUR CODE HERE
 
 # Create a subclass of QueryBase
 # called  `Team`
-#### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
+    name = 'team'
 
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
-        #### YOUR CODE HERE
+        sql = "SELECT team_name, team_id FROM team"
+        return self.query(sql)
     
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
 
         # Query 6
         # Write an SQL query
@@ -39,7 +38,8 @@
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-        #### YOUR CODE HERE
+        sql = f"SELECT team_name FROM team WHERE team_id = {id}"
+        return self.query(sql)
 
 
     # Below is method with an SQL query
@@ -51,16 +51,16 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        sql_string = f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
-                    FROM {self.name}
+                    FROM team
                     JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
+                        USING(team_id)
+                    WHERE team.team_id = {id}
                     GROUP BY employee_id
                    )
                 """
+        return self.pandas_query(sql_string)
