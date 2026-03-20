88c4d4efbe2d3676049cea9d93d8b8b3e392f030
osmelihe <osmanesener@gmail.com>
2026-03-20 22:50:59 +0300

    Add Employee class with names and username methods
    Implemented Employee class methods for retrieving employee names and usernames from the database using SQL queries.

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,25 +1,28 @@
 # Import the QueryBase class
 #### YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 #### YOUR CODE HERE
+from .sql_execution import query
 
 # Define a subclass of QueryBase
 # called Employee
 #### YOUR CODE HERE
-
+class Employee(QueryBase):
+    
     # Set the class attribute `name`
     # to the string "employee"
     #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):   
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -28,14 +31,19 @@
         # This query should return the data
         # for all employees in the database
         #### YOUR CODE HERE
-    
+        sql_query = """
+            SELECT first_name || ' ' || last_name AS full_name,
+                   employee_id
+            FROM employee
+        """
+        return self.query(sql_query)    
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def username(self, id):   
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -43,7 +51,12 @@
         # to only return the full name of the employee
         # with an id equal to the id argument
         #### YOUR CODE HERE
-
+        sql_query = f"""
+            SELECT first_name || ' ' || last_name AS full_name
+            FROM employee
+            WHERE employee_id = {id}
+        """
+        return self.query(sql_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
