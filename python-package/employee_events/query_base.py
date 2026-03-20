83deceaafafcc1e64d4c82c19bcd358e94c14bb1
osmelihe <osmanesener@gmail.com>
2026-03-20 22:59:43 +0300

    Implement QueryBase class for employee events

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,28 +1,32 @@
 # Import any dependencies needed to execute sql queries
 # YOUR CODE HERE
+import pandas as pd
+from .sql_execution import QueryMixin
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
 # YOUR CODE HERE
-
+class QueryBase(QueryMixin):
+    
     # Create a class attribute called `name`
     # set the attribute to an empty string
     # YOUR CODE HERE
-
+    name = ""
     # Define a `names` method that receives
     # no passed arguments
     # YOUR CODE HERE
-        
+    def names(self):
+    
         # Return an empty list
         # YOUR CODE HERE
-
+        return []
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
     # YOUR CODE HERE
-
+    def event_counts(self, id):
         # QUERY 1
         # Write an SQL query that groups by `event_date`
         # and sums the number of positive and negative events
@@ -32,12 +36,23 @@
         # of id columns used for joining
         # order by the event_date column
         # YOUR CODE HERE
-            
+        query1 = f"""
+    SELECT 
+        event_date,
+        SUM(CASE WHEN event_type = 'positive' THEN 1 ELSE 0 END) AS positive_events,
+        SUM(CASE WHEN event_type = 'negative' THEN 1 ELSE 0 END) AS negative_events
+    FROM {self.name}
+    JOIN events ON {self.name}.{self.id_column} = events.{self.id_column}
+    GROUP BY event_date
+    ORDER BY event_date;
+"""
+        return self.pandas_query(sql_query)
     
 
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
     # YOUR CODE HERE
+    def notes(self, id):
 
         # QUERY 2
         # Write an SQL query that returns `note_date`, and `note`
@@ -47,4 +62,12 @@
         # so the query returns the notes
         # for the table name in the `name` class attribute
         # YOUR CODE HERE
-
+        query2 = f"""
+        SELECT 
+            notes.note_date,
+            notes.note
+        FROM {self.name}
+        JOIN notes ON {self.name}.{self.id_column} = notes.{self.id_column}
+        ORDER BY notes.note_date;
+    """
+        return self.pandas_query(sql_query)
